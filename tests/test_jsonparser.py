3adb32d2fc3e2cdc444479265fbe6836eb6fbb77
Simon Yuen <54995458+syuen23@users.noreply.github.com>
2024-04-11 14:57:18 -0400

    Add tests for steps 2, 3, 4

--- tests/test_jsonparser.py
+++ tests/test_jsonparser.py
@@ -6,6 +6,7 @@ from cc_jsonparser.main import compile_json_file
 
 class TestJSONParser:
     runner = CliRunner()
+    BASE_PATH = "./tests/"
 
     def run_single_file(self, filename: os.path) -> bool:
         """Parse a single json file using click's test runner
@@ -28,8 +29,39 @@ class TestJSONParser:
         return not bool(value)
 
     def test_step_one(self):
-        invalid = self.run_single_file("./tests/step1/invalid.json")
-        valid = self.run_single_file("./tests/step1/valid.json")
+        step = "step1/"
+        invalid = self.run_single_file(f"{self.BASE_PATH}{step}invalid.json")
+        valid = self.run_single_file(f"{self.BASE_PATH}{step}valid.json")
 
         assert self.falsy(invalid) == False
         assert self.truthy(valid) == True
+
+    def test_step_two(self):
+        step = "step2/"
+        invalid = self.run_single_file(f"{self.BASE_PATH}{step}invalid.json")
+        invalid2 = self.run_single_file(f"{self.BASE_PATH}{step}invalid2.json")
+        valid = self.run_single_file(f"{self.BASE_PATH}{step}valid.json")
+        valid2 = self.run_single_file(f"{self.BASE_PATH}{step}valid2.json")
+
+        assert self.falsy(invalid) == False
+        assert self.falsy(invalid2) == False
+        assert self.truthy(valid) == True
+        assert self.truthy(valid2) == True
+
+    def test_step_three(self):
+        step = "step3/"
+        invalid = self.run_single_file(f"{self.BASE_PATH}{step}invalid.json")
+        valid = self.run_single_file(f"{self.BASE_PATH}{step}valid.json")
+
+        assert self.falsy(invalid) == False
+        assert self.truthy(valid) == True
+
+    def test_step_four(self):
+        step = "step4/"
+        invalid = self.run_single_file(f"{self.BASE_PATH}{step}invalid.json")
+        valid = self.run_single_file(f"{self.BASE_PATH}{step}valid.json")
+        valid2 = self.run_single_file(f"{self.BASE_PATH}{step}valid2.json")
+
+        assert self.falsy(invalid) == False
+        assert self.truthy(valid) == True
+        assert self.truthy(valid2) == True
